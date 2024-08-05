fa4b8b1e6314e314136c9bd17a4aede33cf5b6fe
Carlos Castellanos <Ccastellanosgarzon@gmail.com>
2024-08-04 21:13:17 -0500

    change rute of  next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === "production" ? "/basepath" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/porvenir" : "",
 };
 
 export default nextConfig;
